3f199fc875543fdf9c90671b3ed2f27f98482bd3
Thiago Ferreira <thiagomf712@gmail.com>
2022-02-16 14:21:43 +0000

    fix footer margin

--- frontend/src/shared/components/Footer/styles.ts
+++ frontend/src/shared/components/Footer/styles.ts
@@ -4,6 +4,7 @@ import { blue } from '@mui/material/colors';
 export const FooterContainer = styled.footer`
   min-height: 4rem;
   width: 100%;
+  margin-top: auto;
 
   background-color: ${blue[700]};
 `;
